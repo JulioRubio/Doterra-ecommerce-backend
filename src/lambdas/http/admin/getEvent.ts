ff86e3727e3e85e66334ade9918295bd24491eec
Jesús Rivera Flores <jesus982009@live.com>
2021-04-07 00:29:37 -0500

    UpdateEvent, getEvent y getEvents

---
+++ src/lambdas/http/admin/getEvent.ts
@@ -0,0 +1,36 @@
+import 'source-map-support/register'
+import * as AWS  from 'aws-sdk'
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const eventsTable = process.env.tableName
+
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    
+    const eventId = event.pathParameters
+    const result = await getEvent(eventId)
+    
+    console.log(event.pathParameters)
+
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin':'*',
+            'Access-Control-Allow-Credentials':true
+        },
+        body: JSON.stringify(result)
+    }
+}
+
+async function getEvent(eventId) {
+    let params = {
+        TableName: eventsTable,
+        Key: eventId
+    }
+
+    const result = await docClient.get(params).promise()
+  
+    return result.Item
+}
---
+++ src/lambdas/http/admin/getEvents.ts
@@ -0,0 +1,34 @@
+import 'source-map-support/register'
+import * as AWS  from 'aws-sdk'
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const eventsTable = process.env.tableName
+
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    
+    const events = await getEvents()
+    
+    console.log(event.pathParameters)
+
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin':'*',
+            'Access-Control-Allow-Credentials':true
+        },
+        body: JSON.stringify(events)
+    }
+}
+
+async function getEvents() {
+    let params = {
+        TableName: eventsTable,
+    }
+
+    const result = await docClient.scan(params).promise()
+  
+    return result.Items
+}
