7f5929e4feb04a6c2df40c953ff988e563cef2a5
Jesús Rivera Flores <jesus982009@live.com>
2021-04-21 10:43:49 -0500

    cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 
 const bodyParser = require('body-parser')
 
@@ -7,6 +8,7 @@ const eventRoutes = require('./routes/events')
 
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.json());
 const PORT = 3000;
 
