0afff66cd7eaafd7b27d0ce48b55704a6691f033
JulioRubio <julioarg97@gmail.com>
2021-05-03 19:03:40 -0500

    changes to index.js

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const express = require('express')
 const cors = require('cors')
+
 const bodyParser = require('body-parser')
 
 const productRoutes = require('./routes/products')
 const eventRoutes = require('./routes/events')
 
 const app = express();
-
-
-
-app.use(bodyParser.json());
-const PORT = 3000;
-
 app.use(cors())
+app.use(bodyParser.json());
+app.use('/products', productRoutes);
+app.use('/events', eventRoutes);
 
+app.options('*',cors())
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
@@ -22,10 +21,7 @@ app.use(function(req, res, next) {
     next();
 });
 
-
-app.use('/products', productRoutes);
-app.use('/events', eventRoutes);
-
+const PORT = 3000;
 
 app.listen(process.env.PORT || PORT,() => {
     console.log(`PORT: ${PORT}}`);
