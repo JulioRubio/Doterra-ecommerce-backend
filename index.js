ed2d710b04151c71e9986f6077c6f682fe33800c
Jesús Rivera Flores <jesus982009@live.com>
2021-05-12 21:55:50 -0500

    Revert "Inicio middleware"
    This reverts commit 6685261a16895657c60ca5505b5a2b51262af3cf.

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
 const express = require('express')
 const cors = require('cors')
-const corss = require('./middleware/cors');
-const { DATABASE_URL, PORT, SECRET_TOKEN } = require('./config');
-
-const middleware = require('./middleware/cors')
-const validateToken = require('./middleware/validateToken');
-const validateSessionToken = require('./middleware/validateSessionToken');
 
 const bodyParser = require('body-parser')
 
 const productRoutes = require('./routes/products')
 const eventRoutes = require('./routes/events')
 const pedidoRoutes = require('./routes/pedidos')
-const clientRoutes = require('./routes/client')
 
 const app = express();
 app.use(cors())
 app.use(bodyParser.json());
-
-app.use(validateToken)
-
-app.use('/products', validateSessionToken, productRoutes);
-app.use('/events', validateSessionToken, eventRoutes);
-app.use('/pedidos', validateSessionToken, pedidoRoutes);
-app.use('/client', middleware, clientRoutes);
+app.use('/products', productRoutes);
+app.use('/events', eventRoutes);
+app.use('/pedidos', pedidoRoutes);
 
 app.options('*',cors())
-app.use(corss);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
+const PORT = 3000;
 
-app.listen(PORT,() => {
+app.listen(process.env.PORT || PORT,() => {
     console.log(`PORT: ${PORT}}`);
 });
 
